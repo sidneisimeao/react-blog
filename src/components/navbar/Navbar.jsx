5ec4cf2b0a1e7a5dd4d2ec8b0ffff3825bdb79e3
sidneisimeao <sidneisimmon@gmail.com>
2018-10-12 10:42:58 -0300

    add NavBar

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+
+document.addEventListener('DOMContentLoaded', () => {
+    // Get all "navbar-burger" elements
+    const navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+    // Check if there are any navbar burgers
+    if (navbarBurgers.length > 0) {
+        // Add a click event on each of them
+        navbarBurgers.forEach(el => {
+            el.addEventListener('click', () => {
+                // Get the target from the "data-target" attribute
+                const target = el.dataset.target;
+                const targetEl = document.getElementById(target);
+                // Toggle the "is-active" className on both the "navbar-burger" and the "navbar-menu"
+                el.classList.toggle('is-active');
+                targetEl.classList.toggle('is-active');
+            });
+        });
+    }
+});
+
+const Navbar = () => (
+    <nav className="navbar" role="navigation" aria-label="main navigation">
+        <div className="navbar-brand">
+            <a className="navbar-item" href="https://bulma.io">
+                <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28" />
+            </a>
+            <a role="button" className="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+            </a>
+        </div>
+        <div id="navbarBasicExample" className="navbar-menu">
+            <div className="navbar-start">
+                <a className="navbar-item">Blog</a>
+                <a className="navbar-item">Momento Humorístico</a>
+                <a className="navbar-item">Momento Filosófico</a>
+            </div>
+            <div className="navbar-end">
+                <div className="navbar-item">
+                    <div className="buttons">
+                        <a className="button is-primary">
+                            <span className="icon">
+                                <i className="fab fa-twitter"></i>
+                            </span>
+                            <span>Twitter</span>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </nav>
+);
+
+export default Navbar;
--- src/layouts/PrimaryLayout.jsx
+++ src/layouts/PrimaryLayout.jsx
@@ -1,22 +1,26 @@
 
-import React from 'react';
+import React, { Fragment } from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 
+import Navbar from 'components/navbar/Navbar';
 import HomePage from 'pages/HomePage';
 import AboutPage from 'pages/AboutPage';
 
 const PrimaryLayout = () => (
-  <div className="main-content">
-    <div className="container ">
-      <div className="columns is-multiline is-mobile is-centered">
-        <Switch>
-          <Route exact path='/' component={HomePage} />
-          <Route path='/about' component={AboutPage} />
-          <Redirect to='/' />
-        </Switch>
+  <Fragment>
+    <Navbar />
+    <div className="main-content">
+      <div className="container ">
+        <div className="columns is-multiline is-mobile is-centered">
+          <Switch>
+            <Route exact path='/' component={HomePage} />
+            <Route path='/about' component={AboutPage} />
+            <Redirect to='/' />
+          </Switch>
+        </div>
       </div>
     </div>
-  </div>
+  </Fragment>
 );
 
 export default PrimaryLayout;
